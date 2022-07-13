3bf0ec31ed7ed1ff24d36292c7d7d612223a8562
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-07-12 22:39:45 -0300

    Aprendendo react e github
    Aprendendo react e github

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world, React!</h1>
+      <h2>Boa noite a todos</h2>
+      
+      <table>
+        <caption>Professo da Tiimi</caption>
+        <tbody>
+          <tr>
+            <td>Italo</td>
+            <td>Guilherme</td>
+            <td>Eduardo</td>
+          </tr>
+          <tr>
+            <td>Terminou</td>
+            <td>Não terminou</td>
+            <td>Não terminou</td>
+          </tr>
+        </tbody>
+      </table>
+
+
     </div>
   );
 }
